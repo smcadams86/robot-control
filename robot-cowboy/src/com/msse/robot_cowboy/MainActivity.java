296e1aadf804c72c11723ba74646caa4535e8bb0
Tyler Smith <tylerhesthedude@gmail.com>
2013-05-05 15:34:52 -0500

    Formatting and organization changes
    order of this class:
    1. fields
    2. overridden methods
    3. public methods
    4. private methods
    5. private classes

--- robot-cowboy/src/com/msse/robot_cowboy/MainActivity.java
+++ robot-cowboy/src/com/msse/robot_cowboy/MainActivity.java
@@ -17,9 +17,7 @@ import android.hardware.usb.UsbManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-import android.os.Looper;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
@@ -46,102 +44,67 @@ import edu.umn.robotcontrol.domain.RobotPosition;
 public class MainActivity extends Activity {
 
   private final String TAG = MainActivity.class.getSimpleName();
+  
   private LocationManager locationManager;
+  
   private UsbSerialDriver mSerialDevice;
   private UsbManager mUsbManager;
+  private SerialInputOutputManager mSerialIoManager;
+  
+  private final ExecutorService mExecutor = Executors
+      .newSingleThreadExecutor();
+  private boolean previewing = false;
+
   private TextView mTitleTextView;
   private TextView mDumpTextView;
   private ScrollView mScrollView;
-  private final ExecutorService mExecutor = Executors
-      .newSingleThreadExecutor();
-  private SerialInputOutputManager mSerialIoManager;
-  private Timer commandPollingTimer = null;
-  private Timer cameraPostingTimer = null;
-  private boolean previewing = false;
-
-  private Camera camera;
   private SurfaceView surfaceView;
   private SurfaceHolder surfaceHolder;
+  
+  private Camera camera;
 
-  LocationListener onLocationChange=new LocationListener() {
-    public void onLocationChanged(Location location) {
-      LocationPoster poster = new LocationPoster();
-      RobotPosition pos = PositionFacade.locToRobotPos(location);
-      poster.execute(buildURL() + "/position", new Gson().toJson(pos));
-    }
+  private Timer commandPollingTimer = null;
+  private Timer cameraPostingTimer = null;
+  
+  LocationListener onLocationChange = new CowboyLocationListener();
+  private OnClickListener snapListener = new OnSnapClickListener();
+  private OnClickListener cameraClickListener = new OnCameraClickListener();
+  private OnClickListener controlClickListener = new OnControlClickListener();
+  private final SerialInputOutputManager.Listener mListener = new CowboySerialDeviceListener();
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-    public void onProviderDisabled(String provider) {
-      // required for interface, not used
-    }
+    PreferenceManager.setDefaultValues(this, R.xml.preferences, true);
+    setContentView(R.layout.activity_main);
 
-    public void onProviderEnabled(String provider) {
-      // required for interface, not used
-    }
+    mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
 
-    public void onStatusChanged(String provider, int status,
-        Bundle extras) {
-      // required for interface, not used
-    }
-  };
+    mTitleTextView = (TextView) findViewById(R.id.logTitle);
+    mDumpTextView = (TextView) findViewById(R.id.logText);
+    mScrollView = (ScrollView) findViewById(R.id.logScroller);
 
+    ImageButton forwardBtn = (ImageButton) findViewById(R.id.forward_btn);
+    ImageButton leftBtn = (ImageButton) findViewById(R.id.left_btn);
+    ImageButton stopBtn = (ImageButton) findViewById(R.id.stop_btn);
+    ImageButton rightBtn = (ImageButton) findViewById(R.id.right_btn);
+    ImageButton backwardBtn = (ImageButton) findViewById(R.id.backward_btn);
+    Button pictureBtn = (Button) findViewById(R.id.photo_btn);
+    Button snapBtn = (Button) findViewById(R.id.snap);
 
-  private final SerialInputOutputManager.Listener mListener = new SerialInputOutputManager.Listener() {
-    @Override
-    public void onRunError(Exception e) {
-      Log.d(TAG, "Runner stopped.");
-    }
+    forwardBtn.setOnClickListener(controlClickListener);
+    leftBtn.setOnClickListener(controlClickListener);
+    stopBtn.setOnClickListener(controlClickListener);
+    rightBtn.setOnClickListener(controlClickListener);
+    backwardBtn.setOnClickListener(controlClickListener);
+    pictureBtn.setOnClickListener(cameraClickListener);
+    snapBtn.setOnClickListener(snapListener);
 
-    @Override
-    public void onNewData(final byte[] data) {
-      MainActivity.this.runOnUiThread(new Runnable() {
-        @Override
-        public void run() {
-          MainActivity.this.updateReceivedData(data);
-        }
-      });
-    }
-  };
-
-  private String buildURL() {
-    String dataSource = PreferenceManager.getDefaultSharedPreferences(this).getString("pref_data_source", "");
-    return "http://" + dataSource + "/control";
+    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
   }
 
   @Override
-  protected void onPause() {
-    super.onPause();
-    stopIoManager();
-    if (mSerialDevice != null) {
-      try {
-        mSerialDevice.close();
-      } catch (IOException e) {
-        // Ignore.
-      }
-      mSerialDevice = null;
-    }
-    stopAsyncTasks();
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.activity_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle item selection
-    switch (item.getItemId()) {
-    case R.id.action_settings:
-      Intent intent = new Intent(this, SettingsActivity.class);
-      startActivity(intent);
-      return true;
-    default:
-      return super.onOptionsItemSelected(item);
-    }
-  }
-
   protected void onResume() {
     super.onResume();
     HashMap<String, UsbDevice> deviceList = mUsbManager.getDeviceList();
@@ -176,6 +139,80 @@ public class MainActivity extends Activity {
     surfaceView = (SurfaceView)findViewById(R.id.surfaceView1);
     surfaceHolder = surfaceView.getHolder();
   }
+  
+  @Override
+  protected void onPause() {
+    super.onPause();
+    stopIoManager();
+    if (mSerialDevice != null) {
+      try {
+        mSerialDevice.close();
+      } catch (IOException e) {
+        // Ignore.
+      }
+      mSerialDevice = null;
+    }
+    stopAsyncTasks();
+  }
+  
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.activity_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle item selection
+    switch (item.getItemId()) {
+    case R.id.action_settings:
+      Intent intent = new Intent(this, SettingsActivity.class);
+      startActivity(intent);
+      return true;
+    default:
+      return super.onOptionsItemSelected(item);
+    }
+  }
+  
+  public void executeCommand(RobotCommand command) {
+    String serialCommand = "S";
+    if (command == null) {
+      return;
+    }
+    switch(command.getComponent()) {
+    case RobotCommand.MOVE_FORWARD:
+      serialCommand = "F";
+      break;
+    case RobotCommand.MOVE_LEFT:
+      serialCommand = "L";
+      break;
+    case RobotCommand.MOVE_REVERSE:
+      serialCommand = "B";
+      break;
+    case RobotCommand.MOVE_RIGHT:
+      serialCommand = "R";
+      break;
+    case RobotCommand.MOVE_STOP:
+      serialCommand = "S";
+      break;
+    }
+
+    Log.v(TAG, "Sending serial command [" + serialCommand + "]");
+
+    try {
+      if (mSerialDevice != null) {
+        mSerialDevice.write(serialCommand.getBytes(), 1000);
+      }
+      else {
+        mDumpTextView.append("\nNo connected Device..");
+        mScrollView.smoothScrollTo(0, mDumpTextView.getBottom());
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+  
   private void stopIoManager() {
     if (mSerialIoManager != null) {
       Log.i(TAG, "Stopping io manager ..");
@@ -205,37 +242,6 @@ public class MainActivity extends Activity {
     mScrollView.smoothScrollTo(0, mDumpTextView.getBottom());
   }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    PreferenceManager.setDefaultValues(this, R.xml.preferences, true);
-    setContentView(R.layout.activity_main);
-
-    mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
-
-    mTitleTextView = (TextView) findViewById(R.id.logTitle);
-    mDumpTextView = (TextView) findViewById(R.id.logText);
-    mScrollView = (ScrollView) findViewById(R.id.logScroller);
-
-    ImageButton forwardBtn = (ImageButton) findViewById(R.id.forward_btn);
-    ImageButton leftBtn = (ImageButton) findViewById(R.id.left_btn);
-    ImageButton stopBtn = (ImageButton) findViewById(R.id.stop_btn);
-    ImageButton rightBtn = (ImageButton) findViewById(R.id.right_btn);
-    ImageButton backwardBtn = (ImageButton) findViewById(R.id.backward_btn);
-    Button pictureBtn = (Button) findViewById(R.id.photo_btn);
-    Button snapBtn = (Button) findViewById(R.id.snap);
-
-    forwardBtn.setOnClickListener(controlClickListener);
-    leftBtn.setOnClickListener(controlClickListener);
-    stopBtn.setOnClickListener(controlClickListener);
-    rightBtn.setOnClickListener(controlClickListener);
-    backwardBtn.setOnClickListener(controlClickListener);
-    pictureBtn.setOnClickListener(cameraClickListener);
-    snapBtn.setOnClickListener(snapListener);
-
-    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-  }
 
   private void startAsyncTasks() {
     long period = Long.valueOf(PreferenceManager
@@ -280,6 +286,7 @@ public class MainActivity extends Activity {
     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
         period, 0, onLocationChange);
   }
+  
   private void stopAsyncTasks() {
     if (commandPollingTimer != null) {
       Log.v(TAG, "Polling timer shutting down...");
@@ -294,52 +301,33 @@ public class MainActivity extends Activity {
     locationManager.removeUpdates(onLocationChange);
   }
 
-  private OnClickListener snapListener = new OnClickListener(){
-    @Override
-    public void onClick(View v){
-      if(!previewing){
-        Toast.makeText(getApplicationContext(), "Start the preview first", Toast.LENGTH_SHORT).show();
-        return;
-      }
-      camera.takePicture(null, null, new Camera.PictureCallback() {
-        @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-          Toast.makeText(getApplicationContext(),
-              "Took Picture, size " + data.length + " bytes",
-              Toast.LENGTH_LONG).show();
-          String FILENAME = "image_file";
-          try {
-            FileOutputStream fos = openFileOutput(FILENAME,
-                Context.MODE_PRIVATE);
-            fos.write(data);
-            fos.close();
-          } catch (Exception e) {
-            Toast.makeText(getApplicationContext(),
-                e.getMessage(), Toast.LENGTH_LONG).show();
-          }
-        }
-      });
+  private String buildURL() {
+    String dataSource = PreferenceManager.getDefaultSharedPreferences(this).getString("pref_data_source", "");
+    return "http://" + dataSource + "/control";
+  }
+  
+  private class CowboyLocationListener implements LocationListener {
+    public void onLocationChanged(Location location) {
+      LocationPoster poster = new LocationPoster();
+      RobotPosition pos = PositionFacade.locToRobotPos(location);
+      poster.execute(buildURL() + "/position", new Gson().toJson(pos));
+    }
+
+    public void onProviderDisabled(String provider) {
+      // required for interface, not used
+    }
+
+    public void onProviderEnabled(String provider) {
+      // required for interface, not used
+    }
+
+    public void onStatusChanged(String provider, int status,
+        Bundle extras) {
+      // required for interface, not used
     }
   };
-
-  private OnClickListener cameraClickListener = new OnClickListener() {
-    @Override
-    public void onClick(View v) {
-      camera = Camera.open();
-      if (camera != null) {
-        try {
-          camera.setDisplayOrientation(90);
-          camera.setPreviewDisplay(surfaceHolder);
-        } catch (IOException e1) {
-          e1.printStackTrace();
-        }
-        camera.startPreview();
-        previewing = true;
-      }
-    }
-  };
-
-  private OnClickListener controlClickListener = new OnClickListener() {
+  
+  private class OnControlClickListener implements OnClickListener{
 
     @Override
     public void onClick(View v) {
@@ -367,42 +355,68 @@ public class MainActivity extends Activity {
       executeCommand(command);
     }
   };
-
-  public void executeCommand(RobotCommand command) {
-    String serialCommand = "S";
-    if (command == null) {
-      return;
-    }
-    switch(command.getComponent()) {
-    case RobotCommand.MOVE_FORWARD:
-      serialCommand = "F";
-      break;
-    case RobotCommand.MOVE_LEFT:
-      serialCommand = "L";
-      break;
-    case RobotCommand.MOVE_REVERSE:
-      serialCommand = "B";
-      break;
-    case RobotCommand.MOVE_RIGHT:
-      serialCommand = "R";
-      break;
-    case RobotCommand.MOVE_STOP:
-      serialCommand = "S";
-      break;
-    }
-
-    Log.v(TAG, "Sending serial command [" + serialCommand + "]");
-
-    try {
-      if (mSerialDevice != null) {
-        mSerialDevice.write(serialCommand.getBytes(), 1000);
+  
+  
+  private class OnCameraClickListener implements OnClickListener {
+    @Override
+    public void onClick(View v) {
+      camera = Camera.open();
+      if (camera != null) {
+        try {
+          camera.setDisplayOrientation(90);
+          camera.setPreviewDisplay(surfaceHolder);
+        } catch (IOException e1) {
+          e1.printStackTrace();
+        }
+        camera.startPreview();
+        previewing = true;
       }
-      else {
-        mDumpTextView.append("\nNo connected Device..");
-        mScrollView.smoothScrollTo(0, mDumpTextView.getBottom());
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
     }
-  }
+  };
+
+  private class OnSnapClickListener implements OnClickListener{
+    @Override
+    public void onClick(View v){
+      if(!previewing){
+        Toast.makeText(getApplicationContext(), "Start the preview first", Toast.LENGTH_SHORT).show();
+        return;
+      }
+      camera.takePicture(null, null, new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+          Toast.makeText(getApplicationContext(),
+              "Took Picture, size " + data.length + " bytes",
+              Toast.LENGTH_LONG).show();
+          String FILENAME = "image_file";
+          try {
+            FileOutputStream fos = openFileOutput(FILENAME,
+                Context.MODE_PRIVATE);
+            fos.write(data);
+            fos.close();
+          } catch (Exception e) {
+            Toast.makeText(getApplicationContext(),
+                e.getMessage(), Toast.LENGTH_LONG).show();
+          }
+        }
+      });
+    }
+  };
+  
+  private class CowboySerialDeviceListener implements SerialInputOutputManager.Listener {
+    @Override
+    public void onRunError(Exception e) {
+      Log.d(TAG, "Runner stopped.");
+    }
+
+    @Override
+    public void onNewData(final byte[] data) {
+      MainActivity.this.runOnUiThread(new Runnable() {
+        @Override
+        public void run() {
+          MainActivity.this.updateReceivedData(data);
+        }
+      });
+    }
+  };
+
 }
