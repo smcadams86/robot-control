18f0f36acf2a4da05b571c279bcb1829e20eabaa
Steven McAdams <smcadams86@gmail.com>
2013-05-07 21:17:23 -0500

    rotate captured photo -90 degrees

--- robot-cowboy/src/com/msse/robot_cowboy/CameraHandler.java
+++ robot-cowboy/src/com/msse/robot_cowboy/CameraHandler.java
@@ -1,6 +1,11 @@
 package com.msse.robot_cowboy;
 
+import java.io.ByteArrayOutputStream;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 
@@ -15,6 +20,24 @@ public class CameraHandler implements PictureCallback {
 
 	@Override
 	public void onPictureTaken(byte[] data, Camera camera) {
-		PhotoQueue.getInstance().add(data);
+		Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+
+		int width = bitmap.getWidth();
+		int height = bitmap.getHeight();
+
+		// create a matrix for the manipulation
+		Matrix matrix = new Matrix();
+		// rotate the Bitmap 90 degrees (counterclockwise)
+		matrix.postRotate(-90);
+
+		// recreate the new Bitmap, swap width and height and apply transform
+		Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0,
+		                  width, height, matrix, true);
+		
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		rotatedBitmap.compress(Bitmap.CompressFormat.PNG, 50, stream);
+		byte[] photoByteArray = stream.toByteArray();
+		
+		PhotoQueue.getInstance().add(photoByteArray);
 	}
 }
